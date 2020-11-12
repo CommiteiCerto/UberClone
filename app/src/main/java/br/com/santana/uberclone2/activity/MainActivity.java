3e9abba4e28c579345be97166805d9f95a8cb578
FelipeHbsis <felipe.msana@gmail.com>
2020-11-12 14:49:21 -0300

    Projeto criado

---
+++ app/src/main/java/br/com/santana/uberclone2/activity/MainActivity.java
@@ -0,0 +1,28 @@
+package br.com.santana.uberclone2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import br.com.santana.uberclone2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+    }
+
+    public void abrirTelaLogin(View view){
+        startActivity( new Intent(this, LoginActivity.class));
+    }
+
+    public void abrirTelaCadastro(View view){
+        startActivity( new Intent(this, CadastroActivity.class));
+    }
+}
---
+++ app/src/main/java/br/com/santana/uberclone2/activity/RequisicoesActivity.java
@@ -0,0 +1,16 @@
+package br.com.santana.uberclone2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.santana.uberclone2.R;
+
+public class RequisicoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requisicoes);
+    }
+}
